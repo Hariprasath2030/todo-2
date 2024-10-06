947bdee194d7e1e5cce4927db7aa1d612adf35b3
Hariprasath V <webhari2005@gmail.com>
2024-10-06 14:50:17 +0530

    Add files via upload

---
+++ src/app/task/task.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient }from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from './task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = "http://localhost:5000/api/tasks";
+
+  constructor(private httpClient: HttpClient) { }
+
+  createTask(newTask: Task):Observable<Task>
+  {
+    return this.httpClient.post<Task>(this.apiUrl, newTask);
+  }
+
+  getAllTasks():Observable<Task[]>
+  {
+    return this.httpClient.get<Task[]>(this.apiUrl);
+  }
+
+  updateTask(taskId: number, updateTask:Task):Observable<Task> {
+    return this.httpClient.put<Task>(this.apiUrl+'/'+taskId, updateTask)
+  }
+
+  deleteTask(taskId: number)
+  {
+    return this.httpClient.delete(this.apiUrl+'/'+taskId);
+  }
+}
